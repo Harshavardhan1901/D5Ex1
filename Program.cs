71f6187c8de5f8b485d41d9687bd9efbd47a3306
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-14 10:09:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D5Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the No of employees: ");
+            int numEmployees = int.Parse(Console.ReadLine());
+
+            int[][] employeeSalaries = new int[numEmployees][];
+
+            for (int i = 0; i < numEmployees; i++)
+            {
+                Console.Write($"Enter the No of working days for Employee {i + 1}: ");
+                int numDays = int.Parse(Console.ReadLine());
+
+                employeeSalaries[i] = new int[numDays];
+
+                Console.WriteLine($"Enter the salary for each day of Employee {i + 1}:");
+                for (int j = 0; j < numDays; j++)
+                {
+                    Console.Write($"Enter Salary of Day {j + 1}: of Employee {i + 1}: ");
+                    employeeSalaries[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nDay\tTotalSalary");
+
+            for (int i = 0; i < numEmployees; i++)
+            {
+                int totalSalary = 0;
+
+                for (int j = 0; j < employeeSalaries[i].Length; j++)
+                {
+                    totalSalary += employeeSalaries[i][j];
+                }
+
+                Console.WriteLine($"Employee {i + 1}\t{totalSalary}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
